b1a3c07cd71a3a94fc2acfb2cdea8a485f937457
Cody Poll <CJPoll@gmail.com>
2014-01-27 23:10:05 -0700

    First shot at a default recipe. Time for testing!

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,12 +2,30 @@
 # Cookbook Name:: sra
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, SIRI
 #
 # All rights reserved - Do Not Redistribute
 #
-git "/vagrant/sra" do
+
+install_dir = '/vagrant/sra'
+
+git install_dir do
 	repository "https://github.com/siricenter/sra"
 	action :sync
 	user "vagrant"
+	not_if {File.exists? "/"}
 end	
+
+bash "run_bundler" do
+	user 'vagrant'
+	cwd install_dir
+	command 'bundle'
+	action :run
+end
+
+bash "run_server" do
+	user 'vagrant'
+	cwd install_dir
+	command 'rails s -p 8000 -d'
+	action :run
+end
