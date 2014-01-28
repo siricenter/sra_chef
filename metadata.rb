ff1a5c7ae1314f181215c2f58c9b2f57f65d892a
Cody Poll <CJPoll@gmail.com>
2014-01-27 22:47:20 -0700

    Added more metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,14 @@
 name             'sra'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Siri'
+maintainer_email 'CJPoll@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures sra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'git'
+
+provides "sra::default"
+provides "sra::production"
+
+supports 'debian', '>= 7.0' 
